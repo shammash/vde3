0985e45df60d499f7135d82520fcf08fb5735e6e
Filippo Giunchedi <filippo@esaurito.net>
2010-01-07 13:18:05 +0100

    add some more tests on component part of context

--- tests/check_context.c
+++ tests/check_context.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+
 #include <check.h>
 #include <vde3.h>
 
-// the fixture context, always present
+// fixture components, always present
 vde_context *f_ctx;
+vde_event_handler f_eh;
 
 void
 setup (void)
 {
   vde_context_new(&f_ctx);
+  vde_context_init(f_ctx, &f_eh);
 }
 
 void
 teardown (void)
 {
+  vde_context_fini(f_ctx);
   vde_context_delete(f_ctx);
 }
 
@@ -24,16 +30,16 @@ START_TEST (test_context_new)
   vde_context *ctx;
 
   rv = vde_context_new(&ctx);
-  fail_unless (rv == 0, "context_new failed");
+  fail_unless (rv == 0, "fail on valid ctx");
 }
 END_TEST
 
-START_TEST (test_context_new_null)
+START_TEST (test_context_new_invalid)
 {
   int rv;
 
   rv = vde_context_new(NULL);
-  fail_unless (rv == -1, "context_new(NULL) didn't fail");
+  fail_unless (rv == -1, "success on ctx == NULL");
 }
 END_TEST
 
@@ -41,22 +47,74 @@ START_TEST (test_context_init)
 {
   int rv;
   vde_event_handler eh;
+  vde_context *ctx;
 
-  rv = vde_context_init(f_ctx, &eh);
-  fail_unless (rv == 0, "context_init failed");
+  vde_context_new(&ctx);
+  rv = vde_context_init(ctx, &eh);
+  fail_unless (rv == 0, "fail on valid arguments");
 }
 END_TEST
 
-START_TEST (test_context_init_null)
+START_TEST (test_context_init_invalid)
 {
   int rv;
   vde_event_handler eh;
+  vde_context *ctx;
 
   rv = vde_context_init(NULL, &eh);
-  fail_unless (rv == -1, "context_init(NULL) didn't fail");
+  fail_unless (rv == -1, "success on null ctx");
 
-  rv = vde_context_init(f_ctx, NULL);
-  fail_unless (rv == -1, "context_init(NULL) didn't fail");
+  rv = vde_context_init(ctx, NULL);
+  fail_unless (rv == -1, "success on null eh");
+}
+END_TEST
+
+START_TEST (test_component_new)
+{
+  int rv;
+  vde_event_handler eh;
+  vde_component *comp;
+
+  rv = vde_context_new_component(f_ctx, VDE_ENGINE, "hub", "test_e", &comp);
+  fail_unless(rv == 0, "new fails on valid arguments %s", strerror(errno));
+
+  rv = vde_context_component_del(f_ctx, comp);
+  fail_unless(rv == 0, "del fails on valid arguments %s", strerror(errno));
+}
+END_TEST
+
+START_TEST (test_component_new_invalid)
+{
+  int rv;
+  vde_event_handler eh;
+  vde_component *comp;
+
+  rv = vde_context_new_component(f_ctx, VDE_ENGINE,
+                                 "thisisnotsupposedtoexists", "test", &comp);
+  fail_unless(rv == -1 && errno == ENOENT, "success on unknown family %s",
+              strerror(errno));
+
+
+  vde_context_new_component(f_ctx, VDE_ENGINE, "hub", "testname", &comp);
+  rv = vde_context_new_component(f_ctx, VDE_ENGINE, "hub", "testname", &comp);
+  fail_unless(rv == -1 && errno == EEXIST,
+              "success on double new component %s", strerror(errno));
+}
+END_TEST
+
+START_TEST (test_component_get)
+{
+  int rv;
+  vde_event_handler eh;
+  vde_component *comp;
+
+  vde_context_new_component(f_ctx, VDE_ENGINE, "hub", "test_e", &comp);
+
+  fail_unless(vde_context_get_component(f_ctx, "test_e") == comp,
+              "fail to return component");
+
+  fail_unless(vde_context_get_component(f_ctx, "") == NULL,
+              "fail to return NULL on empty name");
 }
 END_TEST
 
@@ -69,18 +127,18 @@ context_suite (void)
   TCase *tc_core = tcase_create ("Core");
   tcase_add_checked_fixture (tc_core, setup, teardown);
   tcase_add_test (tc_core, test_context_new);
-  tcase_add_test (tc_core, test_context_new_null);
+  tcase_add_test (tc_core, test_context_new_invalid);
   tcase_add_test (tc_core, test_context_init);
-  tcase_add_test (tc_core, test_context_init_null);
+  tcase_add_test (tc_core, test_context_init_invalid);
   suite_add_tcase (s, tc_core);
 
-  /* Limits test case */
-  /*
-  TCase *tc_limits = tcase_create ("Limits");
-  tcase_add_test (tc_limits, test_money_create_neg);
-  tcase_add_test (tc_limits, test_money_create_zero);
-  suite_add_tcase (s, tc_limits);
-  */
+  /* Component test case */
+  TCase *tc_component = tcase_create ("Component");
+  tcase_add_checked_fixture (tc_component, setup, teardown);
+  tcase_add_test (tc_component, test_component_new);
+  tcase_add_test (tc_component, test_component_new_invalid);
+  tcase_add_test (tc_component, test_component_get);
+  suite_add_tcase (s, tc_component);
   return s;
 }
 
